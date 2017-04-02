af3ef84b5cc273791d410e43681e9298b4aa694e
Tom Short <tshort.rlists@gmail.com>
2017-04-02 14:03:09 -0400

    Hide outpt

--- create-models.sh
+++ create-models.sh
@@ -2,6 +2,6 @@ lein run src/dactyl_keyboard/dactyl.clj
 cp things/right.scad things/right-4x5.scad
 cp things/left.scad things/left-4x5.scad
 cp things/right-plate.scad things/right-4x5-plate.scad
-openscad -o things/right-4x5.stl things/right-4x5.scad &
-openscad -o things/left-4x5.stl  things/left-4x5.scad &
-openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad &
+openscad -o things/right-4x5.stl things/right-4x5.scad  > /dev/null 2>&1 &
+openscad -o things/left-4x5.stl  things/left-4x5.scad > /dev/null 2>&1 &
+openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad > /dev/null 2>&1 &

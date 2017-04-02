000f42799b396cbd09a80ca433e9348e1cb778c3
Tom Short <tshort.rlists@gmail.com>
2017-04-02 17:01:09 -0400

    Another patch attempt

--- create-models.sh
+++ create-models.sh
@@ -6,7 +6,7 @@ openscad -o things/right-4x5.stl things/right-4x5.scad >/dev/null 2>&1 &
 openscad -o things/left-4x5.stl  things/left-4x5.scad >/dev/null 2>&1 &
 openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad >/dev/null 2>&1 &
 
-git am < 0001-Changes-for-5x6.patch 
+patch -p1  5x6.patch 
 lein run src/dactyl_keyboard/dactyl.clj
 cp things/right.scad things/right-5x6.scad
 cp things/left.scad things/left-5x6.scad
@@ -14,7 +14,8 @@ cp things/right-plate.scad things/right-5x6-plate.scad
 openscad -o things/right-5x6.stl things/right-5x6.scad >/dev/null 2>&1  &
 openscad -o things/left-5x6.stl  things/left-5x6.scad >/dev/null 2>&1 &
 openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad >/dev/null 2>&1 &
-git apply -R < 0001-Changes-for-5x6.patch 
+git checkout src/dactyl_keyboard/dactyl.clj
+
 
 # git add things/*-4x5.stl
 # git add things/right-4x5-plate.dxf

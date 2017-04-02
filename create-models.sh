201daa864abbf212a3d52311c7f35109d4f7e92b
Tom Short <tshort.rlists@gmail.com>
2017-04-02 17:11:17 -0400

    Updated patches

--- create-models.sh
+++ create-models.sh
@@ -6,7 +6,7 @@ openscad -o things/right-4x5.stl things/right-4x5.scad >/dev/null 2>&1 &
 openscad -o things/left-4x5.stl  things/left-4x5.scad >/dev/null 2>&1 &
 openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad >/dev/null 2>&1 &
 
-patch -p1  5x6.patch 
+patch -p1 < 5x6.patch 
 lein run src/dactyl_keyboard/dactyl.clj
 cp things/right.scad things/right-5x6.scad
 cp things/left.scad things/left-5x6.scad

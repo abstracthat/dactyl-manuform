d5083932246789a4857b0e61eafe1273801399ee
Tom Short <tshort.rlists@gmail.com>
2017-04-02 14:48:37 -0400

    Adjust screw inserts

--- create-models.sh
+++ create-models.sh
@@ -9,12 +9,12 @@ openscad -o things/right-4x5-plate.dxf things/right-4x5-plate.scad &
 # git add things/right-4x5-plate.dxf
 # git commit -m "Add CAD files"
 
-git checkout 5x6
-lein run src/dactyl_keyboard/dactyl.clj
-cp things/right.scad things/right-5x6.scad
-cp things/left.scad things/left-5x6.scad
-cp things/right-plate.scad things/right-5x6-plate.scad
-openscad -o things/right-5x6.stl things/right-5x6.scad  &
-openscad -o things/left-5x6.stl  things/left-5x6.scad &
-openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad &
-git checkout mine/master
+# git checkout 5x6
+# lein run src/dactyl_keyboard/dactyl.clj
+# cp things/right.scad things/right-5x6.scad
+# cp things/left.scad things/left-5x6.scad
+# cp things/right-plate.scad things/right-5x6-plate.scad
+# openscad -o things/right-5x6.stl things/right-5x6.scad  &
+# openscad -o things/left-5x6.stl  things/left-5x6.scad &
+# openscad -o things/right-5x6-plate.dxf things/right-5x6-plate.scad &
+# git checkout mine/master
